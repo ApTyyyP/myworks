0808b883c86c48232e4bcf4e8c4af1089f5bd642
ApTyyyP <arturhelpme@gmail.com>
2019-05-12 22:01:44 +0300

    Edit game-ui-tabs. Add class active.

--- game-ui-tabs/js/tabs.js
+++ game-ui-tabs/js/tabs.js
@@ -1,22 +1,37 @@
 ;(function ($) {
     $(function () {
-
         $('ul.tabs__caption').each(function (i) {
             var storage = localStorage.getItem('tab' + i);
             if (storage) {
-                $(this).find('li').removeClass('active').eq(storage).addClass('active')
-                    .closest('div.tabs').find('div.tabs__content').removeClass('active').eq(storage).addClass('active');
+                $(this)
+                    .find('li')
+                    .removeClass('active')
+                    .eq(storage).addClass('active')
+                    .closest('nav.tabs')
+                    .find('div.tabs__content')
+                    .removeClass('active')
+                    .eq(storage)
+                    .addClass('active');
             }
         });
 
         $('ul.tabs__caption').on('click', 'li:not(.active)', function () {
+            // console.log($(this).index());
             $(this)
-                .addClass('active').siblings().removeClass('active')
-                .closest('div.tabs').find('div.tabs__content').removeClass('active').eq($(this).index()).addClass('active');
-            var ulIndex = $('ul.tabs__caption').index($(this).parents('ul.tabs__caption'));
+                .addClass('active')
+                .siblings()
+                .removeClass('active')
+                .closest('.top')
+                .next('.bottom')
+                .find('.tabs__content')
+                .removeClass('active')
+                .eq($(this).index())
+                .addClass('active');
+
+            var ulIndex = $('ul.tabs__caption')
+                .index($(this).parents('ul.tabs__caption'));
             localStorage.removeItem('tab' + ulIndex);
             localStorage.setItem('tab' + ulIndex, $(this).index());
         });
-
     });
 })(jQuery);
