5f3b6b93a6c0c1ee5ae7687b56f40ab514ea0e27
ApTyyyP <arturhelpme@gmail.com>
2019-05-24 21:28:52 +0300

    Delete function JS filter-isotop in Foliac

--- foliac/js/main.js
+++ foliac/js/main.js
@@ -63,25 +63,11 @@
             }
         });
 
-        // filter functions
-        var filterFns = {
-            // show if number is greater than 50
-            numberGreaterThan50: function () {
-                var number = $(this).find('.number').text();
-                return parseInt(number, 10) > 50;
-            },
-            // show if name ends with -ium
-            ium: function () {
-                var name = $(this).find('.name').text();
-                return name.match(/ium$/);
-            }
-        };
-
         // bind filter button click
         $('#filters').on('click', 'button', function () {
             var filterValue = $(this).attr('data-filter');
             // use filterFn if matches value
-            filterValue = filterFns[filterValue] || filterValue;
+            filterValue = filterValue || filterValue;
             $grid.isotope({filter: filterValue});
         });
 
