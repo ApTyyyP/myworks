c0d1754104d55c38157a0000c006eb549973f0a7
ApTyyyP <48165186+ApTyyyP@users.noreply.github.com>
2019-05-24 10:28:50 +0300

    Update main.js

--- cahee/js/main.js
+++ cahee/js/main.js
@@ -1,5 +1,22 @@
 ;(function($){
     $(function(){
+        // ѕлавна¤ прокрутка scroll по ¤корю
+        $('a[href^="#"]').on('click', function (event) {
+            // отмен¤ем стандартное действие
+            event.preventDefault();
 
+            var sc = $(this).attr("href"),
+                dn = $(sc).offset().top;
+            /*
+            * sc - в переменную заносим информацию о том, к какому блоку надо перейти
+            * dn - определ¤ем положение блока на странице
+            */
+
+            $('html, body').animate({scrollTop: dn}, 1000);
+
+            /*
+            * 1000 скорость перехода в миллисекундах
+            */
+        });
     });
 })(jQuery);
