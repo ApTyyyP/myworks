facfdc1906529623bc5e5bf4a07b5d42e6b8d6dc
ApTyyyP <arturhelpme@gmail.com>
2019-06-21 03:25:48 +0300

    Add slide 2 (isotop) in Mafia-site.

--- mafia/js/main.js
+++ mafia/js/main.js
@@ -1,7 +1,7 @@
 ;(function ($) {
     $(function () {
 
-// jQuery to collapse the navbar on scroll
+        // jQuery to collapse the navbar on scroll
         $(window).on('scroll', function () {
             if ($(".navbar").offset().top > 180) {
                 $(".navbar-fixed-top").addClass("top-nav-collapse");
@@ -10,9 +10,9 @@
             }
         });
 
-// jQuery for page scrolling feature - requires jQuery Easing plugin
+        // jQuery for page scrolling feature - requires jQuery Easing plugin
         $(function () {
-            $('a.page-scroll').bind('click', function (event) {
+            $('a[href^="#"]').bind('click', function (event) {
                 var $anchor = $(this);
                 $('html, body').stop().animate({
                     scrollTop: $($anchor.attr('href')).offset().top
@@ -21,9 +21,52 @@
             });
         });
 
-// Closes the Responsive Menu on Menu Item Click
-        $('.navbar-collapse ul li a').click(function () {
-            $('.navbar-toggle:visible').click();
+        // Video Popup  //
+        $('.video-popup').magnificPopup({
+            type: 'video'
+        });
+
+        // Wow JS //
+        var wow = new WOW(
+            {
+                boxClass:     'wow',      // animated element css class (default is wow)
+                animateClass: 'animated', // animation css class (default is animated)
+                offset:       0,          // distance to the element when triggering the animation (default is 0)
+                mobile:       false,       // trigger animations on mobile devices (default is true)
+                live:         true,       // act on asynchronously loaded content (default is true)
+                callback:     function(box) {
+                    // the callback is fired every time an animation is started
+                    // the argument that is passed in is the DOM node being animated
+                },
+                scrollContainer: null // optional scroll container selector, otherwise use window
+            }
+        );
+        wow.init();
+
+        // Isotop JS //
+        $(window).on('load', function() {
+
+            if ($.fn.isotope) {
+                $(".isotop-active").isotope({
+                    filter: '*'
+                });
+
+                $('.works-menu ul li').on('click', function() {
+                    $(".works-menu ul li").removeClass("active");
+                    $(this).addClass("active");
+
+                    var selector = $(this).attr('data-filter');
+                    $(".isotop-active").isotope({
+                        filter: selector,
+                        animationOptions: {
+                            duration: 750,
+                            easing: 'easeOutCirc',
+                            queue: false
+                        }
+                    });
+                    return false;
+                });
+            }
         });
 
     });
