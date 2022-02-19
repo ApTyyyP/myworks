c6ace8eccffbb661e8c00a144b52a62025497230
ApTyyyP <arturhelpme@gmail.com>
2022-02-19 14:12:05 +0200

    Editing the site Mafia WOW js. Write comments on main.js in Russian.

--- mafia/js/main.js
+++ mafia/js/main.js
@@ -90,16 +90,16 @@
         // Wow JS //
         var wow = new WOW(
             {
-                boxClass:     'wow',      // animated element css class (default is wow)
-                animateClass: 'animated', // animation css class (default is animated)
-                offset:       0,          // distance to the element when triggering the animation (default is 0)
-                mobile:       false,       // trigger animations on mobile devices (default is true)
-                live:         true,       // act on asynchronously loaded content (default is true)
+                boxClass:     'wow',      // класс, скрывающий элемент до момента отображения на экране (по умолчанию, wow)
+                animateClass: 'animated', // класс для анимации элемента (по умолчанию, animated)
+                offset:       0,          // расстояние в пикселях от нижнего края браузера до верхней границы элемента, необходимое для начала анимации (по умолчанию, 0)
+                mobile:       true,       // включение/отключение WOW.js на мобильных устройствах (по умолчанию, включено)
+                live:         true,       // поддержка асинхронно загруженных элементов (по умолчанию, включена)
                 callback:     function(box) {
-                    // the callback is fired every time an animation is started
-                    // the argument that is passed in is the DOM node being animated
+                    // функция срабатывает каждый раз при старте анимации
+                    // аргумент box — элемент, для которого была запущена анимация
                 },
-                scrollContainer: null // optional scroll container selector, otherwise use window
+                scrollContainer: null // селектор прокручивающегося контейнера (опционально, по умолчанию, window)
             }
         );
         wow.init();
