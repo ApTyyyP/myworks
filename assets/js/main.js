278e11eb7a08f50699978859527eb2164aec354a
ApTyyyP <arturhelpme@gmail.com>
2022-07-17 23:25:15 +0300

    Adding a Bones game.

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,120 +4,193 @@
 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 */
 
-(function($) {
+(function ($) {
 
-	var	$window = $(window),
-		$body = $('body'),
-		$nav = $('#nav');
+    var $window = $(window),
+        $body = $('body'),
+        $nav = $('#nav');
 
-	// Breakpoints.
-		breakpoints({
-			wide:      [ '961px',  '1880px' ],
-			normal:    [ '961px',  '1620px' ],
-			narrow:    [ '961px',  '1320px' ],
-			narrower:  [ '737px',  '960px'  ],
-			mobile:    [ null,     '736px'  ]
-		});
+    // Breakpoints.
+    breakpoints({
+        wide: ['961px', '1880px'],
+        normal: ['961px', '1620px'],
+        narrow: ['961px', '1320px'],
+        narrower: ['737px', '960px'],
+        mobile: [null, '736px']
+    });
 
-	// Play initial animations on page load.
-		$window.on('load', function() {
-			window.setTimeout(function() {
-				$body.removeClass('is-preload');
-			}, 100);
-		});
+    // Play initial animations on page load.
+    $window.on('load', function () {
+        window.setTimeout(function () {
+            $body.removeClass('is-preload');
+        }, 100);
+    });
 
-	// Nav.
-		var $nav_a = $nav.find('a');
+    // Nav.
+    var $nav_a = $nav.find('a');
 
-		$nav_a
-			.addClass('scrolly')
-			.on('click', function(e) {
+    $nav_a
+        .addClass('scrolly')
+        .on('click', function (e) {
 
-				var $this = $(this);
+            var $this = $(this);
 
-				// External link? Bail.
-					if ($this.attr('href').charAt(0) !== '#')
-						return;
+            // External link? Bail.
+            if ($this.attr('href').charAt(0) !== '#')
+                return;
 
-				// Prevent default.
-					e.preventDefault();
+            // Prevent default.
+            e.preventDefault();
 
-				// Deactivate all links.
-					$nav_a.removeClass('active');
+            // Deactivate all links.
+            $nav_a.removeClass('active');
 
-				// Activate link *and* lock it (so Scrollex doesn't try to activate other links as we're scrolling to this one's section).
-					$this
-						.addClass('active')
-						.addClass('active-locked');
+            // Activate link *and* lock it (so Scrollex doesn't try to activate other links as we're scrolling to this one's section).
+            $this
+                .addClass('active')
+                .addClass('active-locked');
 
-			})
-			.each(function() {
+        })
+        .each(function () {
 
-				var	$this = $(this),
-					id = $this.attr('href'),
-					$section = $(id);
+            var $this = $(this),
+                id = $this.attr('href'),
+                $section = $(id);
 
-				// No section for this link? Bail.
-					if ($section.length < 1)
-						return;
+            // No section for this link? Bail.
+            if ($section.length < 1)
+                return;
 
-				// Scrollex.
-					$section.scrollex({
-						mode: 'middle',
-						top: '-10vh',
-						bottom: '-10vh',
-						initialize: function() {
+            // Scrollex.
+            $section.scrollex({
+                mode: 'middle',
+                top: '-10vh',
+                bottom: '-10vh',
+                initialize: function () {
 
-							// Deactivate section.
-								$section.addClass('inactive');
+                    // Deactivate section.
+                    $section.addClass('inactive');
 
-						},
-						enter: function() {
+                },
+                enter: function () {
 
-							// Activate section.
-								$section.removeClass('inactive');
+                    // Activate section.
+                    $section.removeClass('inactive');
 
-							// No locked links? Deactivate all links and activate this section's one.
-								if ($nav_a.filter('.active-locked').length === 0) {
+                    // No locked links? Deactivate all links and activate this section's one.
+                    if ($nav_a.filter('.active-locked').length === 0) {
 
-									$nav_a.removeClass('active');
-									$this.addClass('active');
+                        $nav_a.removeClass('active');
+                        $this.addClass('active');
 
-								}
+                    }
 
-							// Otherwise, if this section's link is the one that's locked, unlock it.
-								else if ($this.hasClass('active-locked'))
-									$this.removeClass('active-locked');
+                    // Otherwise, if this section's link is the one that's locked, unlock it.
+                    else if ($this.hasClass('active-locked'))
+                        $this.removeClass('active-locked');
 
-						}
-					});
+                }
+            });
 
-			});
+        });
 
-	// Scrolly.
-		$('.scrolly').scrolly();
+    // Scrolly.
+    $('.scrolly').scrolly();
 
-	// Header (narrower + mobile).
+    // Header (narrower + mobile).
 
-		// Toggle.
-			$(
-				'<div id="headerToggle">' +
-					'<a href="#header" class="toggle"></a>' +
-				'</div>'
-			)
-				.appendTo($body);
+    // Toggle.
+    $(
+        '<div id="headerToggle">' +
+        '<a href="#header" class="toggle"></a>' +
+        '</div>'
+    )
+        .appendTo($body);
 
-		// Header.
-			$('#header')
-				.panel({
-					delay: 500,
-					hideOnClick: true,
-					hideOnSwipe: true,
-					resetScroll: true,
-					resetForms: true,
-					side: 'left',
-					target: $body,
-					visibleClass: 'header-visible'
-				});
+    // Header.
+    $('#header')
+        .panel({
+            delay: 500,
+            hideOnClick: true,
+            hideOnSwipe: true,
+            resetScroll: true,
+            resetForms: true,
+            side: 'left',
+            target: $body,
+            visibleClass: 'header-visible'
+        });
+
+    // Bones
+	const rots = [
+		{ ry: 0,   rx: 0  }, // 1
+		{ ry: 90,  rx: 0  }, // 2
+		{ ry: 180, rx: 0  }, // 3
+		{ ry: 270, rx: 0  }, // 4
+		{ ry: 0,   rx: 90 }, // 5
+		{ ry: 0,   rx:-90 }  // 6
+	];
+
+	let val = [1,1]
+
+	gsap.set(".face", {
+		position:'absolute',
+		userSelect:'none',
+		width: '100%',
+		height: '100%',
+		rotateY: (i) => rots[i].ry,
+		rotateX: (i) => rots[i].rx,
+		transformOrigin: "50% 50% -150px",
+		z: 150,
+		background:(i)=>'url(images/dieSprite.svg) 0px -'+String(i*300)+'px'
+	});
+
+	let die2 = document.querySelector('.die').cloneNode(true);
+	document.querySelector('.tray').append(die2);
+
+	gsap.set('.die', {attr:{class:(i)=>'die die'+(i+1)}, width:300, height:300, perspective:299});
+	gsap.set('.cube', {position:'absolute', width:300, height:300, transformStyle: 'preserve-3d', z:-600});
+
+	function roll(){
+		val[0] = gsap.utils.random(1,6,1)
+		val[1] = gsap.utils.random(1,6,1)
+
+		gsap.timeline()
+			.to('.sum', {
+				duration:0.17,
+				overwrite:true,
+				opacity:0,
+				ease:'power1.inOut'
+			}, 0)
+
+			.fromTo('.cube', {
+				z:-600
+			},{
+				duration:0.75,
+				z:-300,
+				ease:'expo',
+				yoyoEase:'bounce.out(5)',
+				repeat:1
+			}, 0)
+
+			.fromTo('.cube', {
+				rotationX:(i)=>i==0?'-=360':'+=360' //this ensures that even if the new number is the same, it will do some rotation
+			},{
+				duration:1.5,
+				ease:'back',
+				rotationX:(i)=>-rots[val[i]-1].rx,
+				rotationY:(i)=>-rots[val[i]-1].ry
+			}, 0)
+
+			.set('.sum', {
+				textContent:String(val[0]+val[1])
+			}, 0.8)
+
+			.to('.sum', {
+				opacity:1,
+				ease:'power3.inOut'
+			}, 0.8)
+	}
+
+	window.onpointerup = window.onload = roll;
 
 })(jQuery);
