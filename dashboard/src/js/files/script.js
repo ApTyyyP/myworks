ccafe0b500dd7accddef35f725a0c8ba5692e4f7
Artur <arturhelpme@gmail.com>
2022-09-21 23:14:37 +0300

    Change layout in Pixel Perfect.

--- dashboard/src/js/files/script.js
+++ dashboard/src/js/files/script.js
@@ -4,6 +4,7 @@ import { isMobile } from "./functions.js";
 import { flsModules } from "./modules.js";
 
 const asideLink = document.getElementsByClassName('aside__list-item');
+const paginationItem = document.getElementsByClassName('main__card-pagination-item');
 
 for (let i = 0; i < asideLink.length; i++) {
   asideLink[i].addEventListener("click", function () {
@@ -14,5 +15,17 @@ for (let i = 0; i < asideLink.length; i++) {
     }
 
     this.className += " active";
+  });
+}
+
+for (let i = 0; i < paginationItem.length; i++) {
+  paginationItem[i].addEventListener("click", function () {
+    let current = document.getElementsByClassName("--active");
+
+    if (current.length > 0) {
+      current[0].className = current[0].className.replace(" --active", "");
+    }
+
+    this.className += " --active";
   });
 }
