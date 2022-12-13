cc9dcd8cbee69646c73d61520b8d1b247a372e7b
Artur <arturhelpme@gmail.com>
2022-12-13 18:45:52 +0200

    Component life cycle.

--- react/marvel/src/components/randomChar/RandomChar.js
+++ react/marvel/src/components/randomChar/RandomChar.js
@@ -20,6 +20,10 @@ class RandomChar extends Component {
 
   marvelService = new MarvelService();
 
+  componentWillUnmount() {
+    clearInterval(this.timer);
+  }
+
   onCharLoaded = (char) => {
     this.setState({ char, loading: false });
   };
@@ -54,7 +58,7 @@ class RandomChar extends Component {
             Do you want to get to know him better?
           </p>
           <p className="randomchar__title">Or choose another one</p>
-          <button className="button button__main">
+          <button onClick={this.updateChar} className="button button__main">
             <div className="inner">try it</div>
           </button>
           <img src={mjolnir} alt="mjolnir" className="randomchar__decoration" />
@@ -66,10 +70,23 @@ class RandomChar extends Component {
 
 const View = ({ char }) => {
   const { name, description, thumbnail, homepage, wiki } = char;
+  let imgStyle = { objectFit: 'cover' };
+
+  if (
+    thumbnail ===
+    'http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg'
+  ) {
+    imgStyle = { objectFit: 'contain' };
+  }
 
   return (
     <div className="randomchar__block">
-      <img src={thumbnail} alt="Random character" className="randomchar__img" />
+      <img
+        src={thumbnail}
+        alt="Random character"
+        className="randomchar__img"
+        style={imgStyle}
+      />
       <div className="randomchar__info">
         <p className="randomchar__name">{name}</p>
         <p className="randomchar__descr">{description}</p>
