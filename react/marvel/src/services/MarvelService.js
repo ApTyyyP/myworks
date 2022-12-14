824602e91fba63690657175c61096852868b030a
Artur <arturhelpme@gmail.com>
2022-12-14 09:32:47 +0200

    Changing _baseOffset to offset in MarvelServise.

--- react/marvel/src/services/MarvelService.js
+++ react/marvel/src/services/MarvelService.js
@@ -15,7 +15,7 @@ class MarvelService {
 
   getAllCharacters = async (offset = this._baseOffset) => {
     const res = await this.getResource(
-      `${this._apiBase}characters?limit=9&offset=${this._baseOffset}&${this._apiKey}`
+      `${this._apiBase}characters?limit=9&offset=${offset}&${this._apiKey}`
     );
     return res.data.results.map(this._transformCharacter);
   };
