d3f4715308dacf1ac9881b2c43bdf30633e9a3d3
Artur <arturhelpme@gmail.com>
2023-02-06 15:04:17 +0200

    Redux Toolkit: createSlice()

--- react/hero_admin_panel_template/src/components/heroesFilters/HeroesFilters.js
+++ react/hero_admin_panel_template/src/components/heroesFilters/HeroesFilters.js
@@ -1,21 +1,10 @@
-// Задача для этого компонента:
-// Фильтры должны формироваться на основании загруженных данных
-// Фильтры должны отображать только нужных героев при выборе
-// Активный фильтр имеет класс active
-// Изменять json-файл для удобства МОЖНО!
-// Представьте, что вы попросили бэкенд-разработчика об этом
-
 import { useHttp } from '../../hooks/http.hook';
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import classNames from 'classnames';
 
-import {
-  filtersFetching,
-  filtersFetched,
-  filtersFetchingError,
-  activeFilterChanged,
-} from '../../actions';
+import { fetchFilters } from '../../actions';
+import { filtersChanged } from './filtersSlice';
 import Spinner from '../spinner/Spinner';
 
 const HeroesFilters = () => {
@@ -27,10 +16,8 @@ const HeroesFilters = () => {
 
   // Запрос на сервер для получения фильтров и последовательной смены состояния
   useEffect(() => {
-    dispatch(filtersFetching());
-    request('http://localhost:3001/filters')
-      .then((data) => dispatch(filtersFetched(data)))
-      .catch(() => dispatch(filtersFetchingError()));
+    dispatch(fetchFilters(request));
+
     // eslint-disable-next-line
   }, []);
 
@@ -56,7 +43,7 @@ const HeroesFilters = () => {
           key={name}
           id={name}
           className={btnClass}
-          onClick={() => dispatch(activeFilterChanged(name))}
+          onClick={() => dispatch(filtersChanged(name))}
         >
           {label}
         </button>
