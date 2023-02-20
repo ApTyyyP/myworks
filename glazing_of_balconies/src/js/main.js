b069c4af28f4e1c729b1a60b0286d7ee7244fef6
Artur <arturhelpme@gmail.com>
2023-02-20 23:45:18 +0200

    Working with the calculator form and timer.

--- glazing_of_balconies/src/js/main.js
+++ glazing_of_balconies/src/js/main.js
@@ -2,9 +2,17 @@ import './slider';
 import modals from './modules/modals';
 import tabs from './modules/tabs';
 import forms from './modules/forms';
+import changeModalState from './modules/changeModalState';
+import timer from './modules/timer';
 
 window.addEventListener('DOMContentLoaded', () => {
   'use strict';
+
+  let modalState = {};
+  let deadline = '2023-02-28';
+
+  changeModalState(modalState);
+
   modals();
   tabs('.glazing_slider', '.glazing_block', '.glazing_content', 'active');
   tabs(
@@ -13,5 +21,13 @@ window.addEventListener('DOMContentLoaded', () => {
     '.decoration_content > div > div',
     'after_click'
   );
-  forms();
+  tabs(
+    '.balcon_icons',
+    '.balcon_icons_img',
+    '.big_img > img',
+    'do_image_more',
+    'inline-block'
+  );
+  forms(modalState);
+  timer('.container1', deadline);
 });
